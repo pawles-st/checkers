548679e2fc7ab6ac02d84315d5fd209b83cd4850
pawles <268419@student.pwr.edu.pl>
2022-12-10 09:35:54 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.pawles.checkers.checkers {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.almasb.fxgl.all;
+
+    opens org.pawles.checkers.checkers to javafx.fxml;
+    exports org.pawles.checkers.checkers;
+}
