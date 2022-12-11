1d07343e9fe0459dd226a1da30cffba28f45d0e8
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2022-12-11 11:07:47 +0100

    created server class

---
+++ src/main/java/org/pawles/checkers/server/server.java
@@ -0,0 +1,25 @@
+package org.pawles.checkers.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(1234)) { //try to make a new server at port 1234
+            System.out.println("Server is running 0000");
+
+            Socket socket;
+
+            while(true) {   // this has to be in while(true) because it has to be in loop, so it accepts all users
+                socket = serverSocket.accept();
+                System.out.println("New client connected successfully");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
