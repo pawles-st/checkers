3cbe217817599b0dc91f37279ad13f976cab3556
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2022-12-16 18:39:06 +0100

    abandoned serverThread class and Game class, instead everything is now part of server, added some basic functionality to server, also made testCheckersClient to make some changes and dont destroy the main class

---
+++ src/main/java/org/pawles/checkers/client/testCheckerClient.java
@@ -0,0 +1,89 @@
+package org.pawles.checkers.client;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public final class testCheckerClient {
+
+    // fields for communication with the server
+
+    private static final int PORT = 1234;
+    private static Socket socket;
+    private static Scanner socketIn;
+    private static Writer socketOut;
+
+    private testCheckerClient() { }
+
+    private static void connect() throws IOException {
+
+        // set socket and IO fields for communication
+
+        try {
+            socket = new Socket("localhost", PORT);
+            socketIn = new Scanner(socket.getInputStream());
+            socketOut = new PrintWriter(socket.getOutputStream(), true);
+            System.out.println("connected succesfully");
+        } catch (UnknownHostException e) {
+            throw new UnknownHostException("Couldn't find host");
+        } catch (IOException e) {
+            throw new IOException("Couldn't establish a connection with the server");
+        }
+    }
+
+    private static void joinGame() throws IOException { // TODO: replace RuntimeException
+        try {
+
+            // ask the server to join the game
+
+            socketOut.write("join");
+
+
+            System.out.println("joined succesfully");
+            // confirm the server agreed
+            //if (!socketIn.nextBoolean()) {
+            //    throw new RuntimeException("Couldn't join the game");
+            //}
+        } catch (IOException e) {
+            throw new IOException("Couldn't ask the server to join the game");
+        }
+    }
+
+    private static void await() { // TODO: replace RuntimeException
+        System.out.println("Waiting for the game to begin...");
+        if (!socketIn.nextBoolean()) {
+            throw new RuntimeException("Game couldn't be started");
+        } else {
+            startGame();
+        }
+    }
+
+    private static void startGame() {
+        // TODO: implement game interaction
+    }
+
+    public static void main(final String... args) {
+        try {
+            String message = "siemano";
+            //message = socketIn.nextLine();
+            System.out.println(message);
+
+            connect();
+
+            message = socketIn.nextLine();
+            System.out.println(message);
+
+            joinGame();
+
+            message = socketIn.nextLine();
+            System.out.println(message);
+
+            await();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/org/pawles/checkers/server/server.java
+++ src/main/java/org/pawles/checkers/server/server.java
@@ -1,29 +1,74 @@
 package org.pawles.checkers.server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
 
 public class server {
     public static void main(String[] args) {
-
         try (ServerSocket serverSocket = new ServerSocket(1234)) { //try to make a new server at port 1234
-            System.out.println("Server is running 0000");
-
-            Socket socket;
-            Game game = new Game();
-
-            game.returnBoard();
-
+            System.out.println("Server is running at port 1234");       //confirm that the server runs at given port
+            var pool = Executors.newFixedThreadPool(200);
             while(true) {   // this has to be in while(true) because it has to be in loop, so it accepts all users
-                socket = serverSocket.accept();
-                System.out.println("New client connected successfully");
-                new serverThread(socket).start();
+                GameBoard gameBoard = new GameBoard();                  //create new gameboard
+                pool.execute(gameBoard.new Player(serverSocket.accept(), "White"));  //accept White Player
+                pool.execute(gameBoard.new Player(serverSocket.accept(), "Black"));  //accept Black player
             }
-
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
             ex.printStackTrace();
+        }
+    }
+
+    static class GameBoard {
+        private int[][] board = new int[8][8];     //declare board
+        Player currentPlayer;
+
+        public boolean enemyLost() {
+            return (currentPlayer.opponent.pawns==0);
+        }
+
+        public synchronized void move() {
+            if(currentPlayer.opponent == null) {
+                throw new IllegalStateException("You don't have an opponent yet");
+            }
+
+            // TODO movement!!!
+
+            currentPlayer = currentPlayer.opponent; // switch current player
+        }
+
+        class Player extends Thread {
+            public int pawns;
+            Player opponent;
+            Socket socket;
+            Scanner input;
+            Writer output;
+            String name;
+
+            public Player(Socket socket, String name) {
+                this.socket = socket;
+                this.name = name;
+                pawns = 12;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    setup();
+                } catch (IOException ioex) {
+                    System.out.println("Setup IOException");
+                }
+            }
+
+            private void setup() throws IOException {
+                input = new Scanner(socket.getInputStream());
+                output = new PrintWriter(socket.getOutputStream());
+            }
         }
     }
 }
