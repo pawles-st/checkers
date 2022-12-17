34720085e054ca1e9aeac70b4cbae35a0328fc84
pawles <268419@student.pwr.edu.pl>
2022-12-17 17:33:47 +0100

    Fixed move check in Man class

--- src/main/java/org/pawles/checkers/objects/Man.java
+++ src/main/java/org/pawles/checkers/objects/Man.java
@@ -7,8 +7,8 @@ public class Man extends Piece {
 
     @Override
     public void move(Square curr, Square dest) {
-        int diff_x = curr.getX() - dest.getX();
-        if (diff_x != curr.getY() - dest.getY() || diff_x > 2) {
+        int diff_x = Math.abs(curr.getX() - dest.getX());
+        if (diff_x != Math.abs(curr.getY() - dest.getY()) || diff_x > 2) {
             // illegal move, throw exception
         } else {
             square.setX(dest.getX());
