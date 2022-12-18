67a70226149d891be97e0586c96dce6ee99cb782
pawles <268419@student.pwr.edu.pl>
2022-12-18 11:07:54 +0100

    Added ClientController class

---
+++ src/main/java/org/pawles/checkers/client/ClientController.java
@@ -0,0 +1,26 @@
+package org.pawles.checkers.client;
+
+import org.pawles.checkers.objects.Piece;
+import org.pawles.checkers.objects.Square;
+
+public class ClientController {
+    final private ClientModel model;
+    final private ClientView view;
+
+    public ClientController(ClientModel model, ClientView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setCoordinate(Square square, Piece piece) {
+        model.setCoordinate(square, piece);
+    }
+
+    public void movePiece(Square curr, Square dest) {
+        model.movePiece(curr, dest);
+    }
+
+    public void updateView() {
+        view.drawBoard(model.getBoard());
+    }
+}
