1a9b4f7bedc0712a2ab9137090e94ff0495ea8d8
pawles <268419@student.pwr.edu.pl>
2022-12-18 09:04:33 +0100

    Added the ClientView class to print what the user sees

---
+++ src/main/java/org/pawles/checkers/client/ClientView.java
@@ -0,0 +1,43 @@
+package org.pawles.checkers.client;
+
+import org.pawles.checkers.objects.Board;
+import org.pawles.checkers.objects.Colour;
+import org.pawles.checkers.objects.Piece;
+
+import java.util.ArrayList;
+
+public class ClientView { // TODO: implement different view based on the colour of the player
+    
+    private char convertTileToChar(Piece piece) {
+        if (piece == null) {
+            return ' ';
+        } else if (piece.getColour() == Colour.WHITE) {
+            return 'O';
+        } else {
+            return 'X';
+        }
+    }
+    
+    public void drawBoard(Board board) {
+
+        final String hWall = "+-+-+-+-+-+-+-+-+";
+
+        ArrayList<ArrayList<Piece>> coordinates = board.getCoordinates();
+
+        System.out.println(hWall);
+
+        for (int y = 0; y < 8; ++y) {
+
+            System.out.println('|');
+
+            for (int x = 0; x < 8; ++x) {
+
+                System.out.print(convertTileToChar(coordinates.get(y).get(x)));
+                System.out.print('|');
+
+            }
+
+            System.out.println(hWall);
+        }
+    }
+}
