a750f57028df8c77700e2700acd7734b487807c0
pawles <268419@student.pwr.edu.pl>
2022-12-21 21:01:38 +0100

    GameCommunicator does basic validation of the move before sending it

--- src/main/java/org/pawles/checkers/client/GameCommunicator.java
+++ src/main/java/org/pawles/checkers/client/GameCommunicator.java
@@ -1,6 +1,7 @@
 package org.pawles.checkers.client;
 
 import org.pawles.checkers.objects.Colour;
+import org.pawles.checkers.objects.Square;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -36,13 +37,51 @@ public class GameCommunicator {
             final String msg = socketIn.nextLine();
             if ("your turn".equals(msg)) {
                 myTurn = true;
+            } else {
+                throw new RuntimeException("Invalid message received");
             }
-            String verification;
+
+            // show opponent's move
+
+            clientController.updateView();
+
+            // single turn (can be composed of multiple moves)
+
+            String verification = "";
+            Square curr;
+            Square dest;
             do {
+
+                if ("incorrect".equals(verification)) {
+                    System.out.println("Incorrect move, try again.");
+                }
+
+                // ask player for the move
+
                 System.out.print("Move from: ");
                 final String from = clientIn.nextLine();
                 System.out.print("Move to: ");
                 final String to = clientIn.nextLine();
+
+                // convert move to Squares
+
+                int curr_x = Integer.parseInt(String.valueOf(from.charAt(0)));
+                int curr_y = Integer.parseInt(String.valueOf(from.charAt(1)));
+                curr = new Square(curr_x, curr_y); // store squares somewhere to avoid constant creation???
+                int dest_x = Integer.parseInt(String.valueOf(to.charAt(0)));
+                int dest_y = Integer.parseInt(String.valueOf(to.charAt(1)));
+                dest = new Square(dest_x, dest_y);
+
+                // check
+
+                if (!clientController.verifyMove(curr, dest)) {
+                    clientController.updateView();
+                    System.out.println("Incorrect move, try again.");
+                    continue;
+                }
+
+                // sent the move to the server to verify
+
                 final String move = from + ":" + to;
                 socketOut.println(move);
                 verification = socketIn.nextLine();
