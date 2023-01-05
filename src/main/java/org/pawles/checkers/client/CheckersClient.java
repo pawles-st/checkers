d62d05b8b7f0ac0f155dc8b278e07ab2c61bc6c6
pawles <268419@student.pwr.edu.pl>
2023-01-05 10:06:00 +0100

    Fixed PMD errors in CheckersClient and added documentation

--- src/main/java/org/pawles/checkers/client/CheckersClient.java
+++ src/main/java/org/pawles/checkers/client/CheckersClient.java
@@ -1,5 +1,6 @@
 package org.pawles.checkers.client;
 
+import org.pawles.checkers.exceptions.CannotStartGameException;
 import org.pawles.checkers.objects.Colour;
 
 import java.io.IOException;
@@ -8,16 +9,35 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+/**
+ * Main class to run from the client side
+ *
+ * @author pawles
+ * @version 1.0
+ */
 public final class CheckersClient {
 
     // fields for communication with the server
 
+    /** number of the port to connect to */
     private static final int PORT = 1234;
-    private static Socket socket;
+
+    /** input stream for the server socket */
     private static Scanner socketIn;
+
+    /** output stream for the server socket */
     private static PrintWriter socketOut;
-    private static GameCommunicator gameCommunicator;
+
+    /** player colour for the game */
     private static Colour colour;
+
+    // client message streams
+
+    /** output stream for client messages */
+    private static final PrintWriter OUT = new PrintWriter(System.out);
+
+    /** error stream for client messages */
+    private static final PrintWriter ERR = new PrintWriter(System.err);
 
     private CheckersClient() { }
 
@@ -25,24 +45,21 @@ public final class CheckersClient {
 
         // set socket and IO fields for communication
 
-        try {
-            socket = new Socket("localhost", PORT);
+        try (Socket socket = new Socket("localhost", PORT)) {
             socketIn = new Scanner(socket.getInputStream());
             socketOut = new PrintWriter(socket.getOutputStream(), true);
         } catch (UnknownHostException e) {
-            throw (UnknownHostException) new UnknownHostException().initCause(e);
-            //throw new UnknownHostException("Couldn't find host");
+            throw (UnknownHostException) new UnknownHostException("Server doesn't exist").initCause(e);
         } catch (IOException e) {
-            throw (IOException) new IOException().initCause(e);
-            //throw new IOException("Couldn't establish a connection with the server");
+            throw new IOException("couldn't establish a connection to the server", e);
         }
     }
 
-    private static void await() { // TODO: replace RuntimeException
+    private static void await() {
 
         // print the waiting message
 
-        System.out.println("Waiting for the game to begin...");
+        OUT.println("Waiting for the game to begin...");
 
         // when server returns the colour, finish waiting
 
@@ -52,7 +69,7 @@ public final class CheckersClient {
         } else if ("Black".equals(col)) {
             colour = Colour.BLACK;
         } else {
-            throw new RuntimeException("Game couldn't be started");
+            throw new CannotStartGameException("Couldn't receive the player's colour from the server");
         }
     }
 
@@ -60,17 +77,22 @@ public final class CheckersClient {
 
         // start the GameCommunicator class which handles the game from client side
 
-        gameCommunicator = new GameCommunicator(colour, socketIn, socketOut);
+        final GameCommunicator gameCommunicator = new GameCommunicator(colour, socketIn, socketOut);
         gameCommunicator.start();
     }
 
+    /**
+     * connects to the server and initiates tho client side
+     *
+     * @param args arguments (should be left empty)
+     */
     public static void main(final String... args) {
         try {
             connect();
             await();
             startGame(colour);
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            ERR.println(e.getMessage());
         }
     }
 }
