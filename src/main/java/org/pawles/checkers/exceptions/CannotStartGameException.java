aabfaf267d7f70b02c31f3194f9601cecbd8caef
pawles <268419@student.pwr.edu.pl>
2023-01-05 10:43:18 +0100

    Fixed PMD errors in CannotStartGameException and added documentation

--- src/main/java/org/pawles/checkers/exceptions/CannotStartGameException.java
+++ src/main/java/org/pawles/checkers/exceptions/CannotStartGameException.java
@@ -1,7 +1,21 @@
 package org.pawles.checkers.exceptions;
 
+import java.io.Serial;
+
+/**
+ * Exception class to inform the client that the game cannot start
+ * @author pawles
+ * @version 1.0
+ */
 public class CannotStartGameException extends RuntimeException {
-    public CannotStartGameException(String message) {
+    @Serial
+    private static final long serialVersionUID = 10L;
+
+    /**
+     * default constructor
+     * @param message exception message
+     */
+    public CannotStartGameException(final String message) {
         super(message);
     }
 }
