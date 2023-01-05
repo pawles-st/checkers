434c29c5d0b9ac822f8c7b5101551c706c3ccc07
pawles <268419@student.pwr.edu.pl>
2023-01-05 10:23:38 +0100

    Fixed PMD errors in ClientModel and added documentation

--- src/main/java/org/pawles/checkers/client/ClientModel.java
+++ src/main/java/org/pawles/checkers/client/ClientModel.java
@@ -7,10 +7,24 @@ import org.pawles.checkers.objects.Square;
 import org.pawles.checkers.utils.BoardDirector;
 import org.pawles.checkers.utils.BrazilianBoardBuilder;
 
+/**
+ * MVC model class for the client side
+ *
+ * @author pawles
+ * @version 1.0
+ */
 public class ClientModel {
+
+    /** game board */
     final private Board board;
+
+    /** player's colour */
     final private Colour colour;
 
+    /**
+     * initialise the MVC client's model
+     * @param colour player's colour
+     */
     public ClientModel(final Colour colour) {
 
         // build board
@@ -25,22 +39,46 @@ public class ClientModel {
         this.colour = colour;
     }
 
+    /**
+     * return current board state
+     * @return current board
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * return player's colour
+     * @return player's colour
+     */
     public Colour getColour() {
         return colour;
     }
 
+    /**
+     * place a piece at a given coordinate
+     * @param square the square on the board to modify
+     * @param piece the piece to place
+     */
     public void setCoordinate(final Square square, final AbstractPiece piece) {
         board.setCoordinate(square, piece);
     }
 
+    /**
+     * check whether the piece has the ability to move between squares
+     * @param curr current square
+     * @param dest destination square
+     * @return true if the move is correct; false otherwise
+     */
     public boolean verifyMove(final Square curr, final Square dest) {
         return board.verifyMove(curr, dest);
     }
 
+    /**
+     * change the position of a single piece on the board
+     * @param curr current square
+     * @param dest destination square
+     */
     public void movePiece(final Square curr, final Square dest) {
         board.movePiece(curr, dest);
     }
